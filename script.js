0c7e64f44aa9433ac61b46aa1890152d72feab80
Anna Cyganowski <pallasathena@Meep.local>
2014-07-31 13:20:02 -0700

    Cleaned up some code

--- script.js
+++ script.js
@@ -82,6 +82,7 @@ function sensorTest(){
             $(this).text('start').removeClass('stop_button');
             // Insert what happens when user presses stop button
             gyro.stopTracking();
+            // Display last collected raw data point upon pressing 'stop'
             document.getElementById("sensor_data").innerHTML = 
                 "Tracking stopped. <br/>" 
                 + "x: " + sensorData.x[sensorData.x.length-1] + "<br/>"
@@ -97,6 +98,7 @@ function sensorTest(){
             gyro.frequency = 10;
             milliseconds = 0;
             gyro.startTracking(function(o) {
+                // Display raw data in text format (will probably remove this later)
                 document.getElementById("sensor_data").innerHTML =
                     "x: " + o.x + "<br/>"
                     + "y: " + o.y + "<br/>"
@@ -105,6 +107,7 @@ function sensorTest(){
                     + "beta: " + o.beta + "<br/>" 
                     + "gamma: " + o.gamma;
                 
+                    // Push data to local JS object for easy reference (will probably remove this later)
                     sensorData.milliseconds.push(milliseconds);
                     sensorData.x.push(o.x);
                     sensorData.y.push(o.y);
@@ -113,6 +116,7 @@ function sensorTest(){
                     sensorData.beta.push(o.beta);
                     sensorData.gamma.push(o.gamma);
 
+                    // Push data to Firebase reference
                     firebaseSensorData.push({
                         milliseconds: [milliseconds],
                         x: [o.x],
@@ -123,6 +127,8 @@ function sensorTest(){
                         gamma: [o.gamma]
                     });
 
+                    // Time is measured incrementally, not using datetime - might not be accurate
+                    // Should I implement this differently?
                     milliseconds = milliseconds + gyro.frequency;
             });
         }
@@ -226,31 +232,4 @@ function sensorPlot(){
 
         update();
 
-
-    // Example simple, resizeable Flot chart
-
-    /*
-
-    $(function () {
-        var x = [[0, -3.761590156], [10, -3.761590156], [20, -3.761590156], [30, -3.761590156], [40, -0.726490566], 
-                    [50, -0.726490566], [60,-0.726490566], [70, -0.726490566], [80, -3.806630856], [90, -3.806630856],
-                    [100, -3.806630856], [110, -3.806630856], [120, -3.806630856], [130, -4.948665048]];
-
-        var y = [[0, -9.103203469], [10, -9.103203469], [20, -9.103203469], [30, -9.103203469], [40, -16.60229214],
-                    [50, -16.60229214], [60, -16.60229214], [70, -16.60229214], [80, -8.17574961], [90, -8.17574961], 
-                    [100, -8.17574961], [110, -8.17574961], [120, -8.17574961], [130, -7.631817067]];
-     
-        var z = [[0, 4.644751221], [10, 4.644751221], [20, 4.644751221], [30, 4.644751221], [40, 0.161608612],
-                    [50, 0.161608612], [60, 0.161608612], [70, 0.161608612], [80, 4.532822293], [90, 4.532822293],
-                    [100, 4.532822293], [110, 4.532822293], [120, 4.532822293], [130, 0.473752507]];
-        
-        $.plot($("#placeholder"), [ x, y, z ]);
-        
-        window.onresize = function(event) {
-            $.plot($("#placeholder"), [ x, y, z ]);
-        }
-
-
-    });
-    */
 }
